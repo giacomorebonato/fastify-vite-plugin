93bc5f966cef0476173f9b1d8a03361479d144ce
Giacomo Rebonato <grebonato@outlook.ie>
2023-07-02 16:05:10 +0100

    file renamed

--- examples/server.js
+++ examples/server.js
@@ -2,7 +2,7 @@ import Fastify from 'fastify'
 
 const fastify = Fastify()
 
-await fastify.register(import('../dist/vite-plugin.js'))
+await fastify.register(import('../dist/fastify-vite-plugin.js'))
 await fastify.listen({ port: 3000 })
 
 console.log('Server running on http://localhost:3000')
